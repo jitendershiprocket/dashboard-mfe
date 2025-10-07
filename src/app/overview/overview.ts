a08de0d5cffbbcdb1863404a9906c5d521c69bde
jitendershiprocket <jitender.sharma@kartrocket.com>
2025-10-07 16:41:01 +0530

    working on dashboard

--- src/app/overview/overview.ts
+++ src/app/overview/overview.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef, signal, computed, afterNextRender } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpService } from '../services/http-service.service';
